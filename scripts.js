cd4b0cf9e8be2fd65ebc023a18a6eef0af63cfc3
Evan Norton <evanan39@yahoo.com>
2018-10-12 15:36:20 -0400

    bug fix

--- scripts.js
+++ scripts.js
@@ -1,11 +1,14 @@
 (function ($) {
+    
+    $(document).ready(function() {
+        $($("form")[0]).submit(function(e) {
+            let val = $("form")[0][0].value;
+            if (!val) {
+                e.preventDefault();
+                alert("Upload a Lighthouse Audit JSON file first");
+            }
+        });
+    })
 
-    $($("form")[0]).submit(function(e) {
-        let val = $("form")[0][0].value;
-        if (!val) {
-            e.preventDefault();
-            alert("Upload a Lighthouse Audit JSON file first");
-        }
-    });
 
 })(jQuery);
